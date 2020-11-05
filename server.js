51f79db03cf2988c6985fd8f18bfca312428729a
Clark Freifeld <ccf@ccs.neu.edu>
2020-11-04 23:11:44 -0500

    fix dir

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/wbdv-angular-demo'));
+app.use(express.static(__dirname + '/dist/wbdv-angular-demo/src'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/wbdv-angular-demo/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/wbdv-angular-demo/src/index.html'));
 });
 app.listen(process.env.PORT || 8080);
